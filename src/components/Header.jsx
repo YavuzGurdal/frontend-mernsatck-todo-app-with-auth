786b93376a5b2a08ab27f4f695ab47a3fc8d455c
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-08-13 12:04:55 +0200

    logout added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,48 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { logout, reset } from '../redux/auth/authSlice'
 
 
 const Header = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { user } = useSelector((state) => state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
     return (
         <div className="header">
             <Link to='/'>
                 <div className="logo">TodoApp</div>
             </Link>
             <ul>
-                <Link to='/signup'>
-                    <li>
-                        <FaUser />SignUp
-                    </li>
-                </Link>
-                <Link to='/login'>
-                    <li>
-                        <FaSignInAlt />Login
-                    </li>
-                </Link>
-                <li>
-                    <FaSignOutAlt /> Logout
-                </li>
+                {
+                    user ?
+                        (
+                            <li onClick={onLogout}>
+                                <FaSignOutAlt /> Logout
+                            </li>
+                        )
+                        :
+                        (
+                            <>
+                                <Link to='/signup'>
+                                    <li>
+                                        <FaUser />SignUp
+                                    </li>
+                                </Link>
+                                <Link to='/login'>
+                                    <li>
+                                        <FaSignInAlt />Login
+                                    </li>
+                                </Link>
+                            </>
+                        )
+                }
             </ul>
         </div>
     )
--- src/redux/auth/authService.js
+++ src/redux/auth/authService.js
@@ -13,8 +13,19 @@ const login = async (userData) => {
     return response.data
 }
 
+//Logout User (axios)
+const logout = async () => {
+    const response = await axios.get(API_URL + "logout")
+
+    if (response.data) {
+        localStorage.removeItem('user_todo')
+    }
+
+    return response.data
+}
+
 const authService = {
-    login
+    login, logout
 }
 
 export default authService
