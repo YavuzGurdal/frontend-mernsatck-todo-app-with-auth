b5b4611df40277563c088eecb1d92b8131f3a29e
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-08-15 22:53:09 +0200

    withCredentials false

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 export const axiosInstance = axios.create({ // bunu surekli ayni seyi yazmamak icin yazdim
     //baseURL: 'http://localhost:8080',
     baseURL: 'https://dashboard.heroku.com/apps/mern-todoapp-withauth-be',
-    withCredentials: true, // cookie icin
+    withCredentials: false, // cookie icin
 })
